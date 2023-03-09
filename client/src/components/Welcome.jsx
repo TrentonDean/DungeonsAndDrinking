45c7ca6da27e35ba39831db680d5d5c955ca231e
Trenton Dean <trentondean@icloud.com>
2023-03-09 09:20:49 -0600

    began work on develop branch

---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Welcome = () => {
+
+
+    return(
+        <div className="welcome">
+            <div className="wel-header">
+                <h2>Welcome Adventurers!</h2>
+                <p>Prepare to embark on a journey of grand proportions and even grander drinking! Grab your drink of choice and let's begin!</p>
+            </div>
+            <div className="player-num">
+                <h4>How many adventurers are in your party?</h4>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Welcome
